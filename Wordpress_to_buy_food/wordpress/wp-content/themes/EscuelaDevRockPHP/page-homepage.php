bd0a36a3f36b9b5d94071ad7b62a5c1498dde78f
@LeoAchear <leoachear@gmail.com>
2016-08-02 22:10:03 -0300

    Color picker

--- Wordpress_to_buy_food/wordpress/wp-content/themes/EscuelaDevRockPHP/page-homepage.php
+++ Wordpress_to_buy_food/wordpress/wp-content/themes/EscuelaDevRockPHP/page-homepage.php
@@ -82,4 +82,113 @@ get_header(); ?>
 			</div>
 		</div>
 	</div>
+
+<section class="cursos">
+    <div id='swift' style="background:  linear-gradient(to left, <?php echo types_render_field('primer-color'); ?>, <?php echo types_render_field('segundo-color'); ?>">
+        <div class="container">
+            <div class="row espacios">
+                <div class="col-md-2">
+                    <a class='muestra' href="#swift"><img alt="" class="logo" src=
+                    "<?php echo get_template_directory_uri(); ?>/img/swift.png"></a>
+                </div>
+                <div class="col-md-10">
+                    <h1 class="display-3">Swift Ninja</h1>
+                    <p class="lead">Para devs interesados
+                    en aprender a desarrollar <br> aplicaciones en iOS tvOS.
+                    watchOS.</p><a class="btn btn-primary muestra" type=
+                    "submit" href='#swift'>Quiero + info</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='container'>
+        <div class="shows espacios">
+            <div class="col-md-2"></div>
+            <div class="col-md-10">
+                <div class="media">
+                    <a class="media-left" href="#"><img alt="@f0go" class=
+                    "media-object" src=
+                    "https://pbs.twimg.com/profile_images/705130099058851840/XTwiZkV6.jpg"></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Profesor: @f0go</h4>
+                        <p>F0go es un amante del desarrollo, que se especializó
+                        en iOS desde sus inicios, abarcando grandes e
+                        importantes proyectos. Si utilizaste iOS en algún
+                        momento, seguramente usaste una aplicación que realizó.
+                        <a href="http://www.dev-f0go.com/p/projects.html">Aqui algunos de sus proyectos</a></p>
+                    </div>
+                </div>
+                <div class="media">
+                    <a class="media-left" href="#"><i class=
+                    "material-icons">&#xE55F;</i></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Lugar</h4>
+                        <p><a href=
+                        "https://www.google.com.ar/maps/place/Escuela+Dev+Rock/@-34.5945006,-58.4039817,17z/data=!3m1!4b1!4m2!3m1!1s0x95bcca909524474b:0x17ec12e7da43dfad?hl=es-419">
+                        <p>Av. Santa Fe 2459, CABA<br>
+                        Ciudad Autónoma de Buenos Aires<br>
+                        </p></a></p>
+                    </div>
+                </div>
+                <div class="media">
+                    <a class="media-left" href="#"><i class=
+                    "material-icons">&#xE858;</i></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Duración</h4>
+                        <p><strong>Horas:</strong> 30hs.<br>
+                        <strong>Inicio:</strong> Mayo 2016<br>
+                        <strong>Fin:</strong> Junio 2016<br>
+                        <strong>Día de cursada:</strong> Jueves, 19 a 22hs.</p>
+                    </div>
+                </div>
+                <div class="media">
+                    <a class="media-left" href="#"><i class=
+                    "material-icons">&#xE925;</i></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Preguntas Frecuentes</h4>
+                        <p><strong>¿Modalidad?</strong><br>
+                        Presencial<br>
+                        <strong>¿Requisitos?</strong><br>
+                        Traer computadora<br>
+                        <strong>¿Requiere conocimientos previos?</strong><br>
+                        No son necesarios, pero lo ideal es tener una noción de
+                        programación orientada a objetos.<br>
+                        <strong>¿A quién se orienta este curso?</strong><br>
+                        A internautas o desarrolladores interesados en aprender
+                        a desarrollar aplicaciones en iOS tvOS watchOS.<br>
+                        <strong>¿Qué tipo de diploma de certificación
+                        entregan?</strong><br>
+                        El certificado demuestra que has aprendido y
+                        comprendido exitosamente los temas vistos. Avalado por
+                        managers de Google Developer Group Río de la Plata.<br>
+                        <strong>Para los que vivimos lejos, ¿se puede hacer
+                        online?</strong><br>
+                        Sí, podes cursarlo online en vivo en el mismo horario
+                        que los alumnos presenciales, te conectas con el curso
+                        y lo tomas desde tu casa.<br>
+                        <br>
+                        <a class="btn btn-primary-outline online"  data-nombre="Swift Ninja">Consultar Precios Modalidad Online</a></p>
+                    </div>
+
+                </div>
+                <div class="media">
+                    <a class="media-left" href="#"><i class=
+                    "material-icons">&#xE8CC;</i></a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Valor del curso</h4>
+                        <h4 class="media-heading">$4.000</h4>
+                        <p><strong>Efectivo:</strong> 10% de descuento<br>
+                        <strong>Tarjetas:</strong> 3/6 cuotas sin interes con
+                        MP.<br></p>
+                        <a mp-mode="dftl" href="https://www.mercadopago.com/mla/checkout/start?pref_id=133819650-cb6f38b0-9044-42db-8361-f751e1ed6de7" name="MP-payButton" class='btn btn-primary-outline'>Pagar con Mercado Pago</a>
+                        <span class="MP-aron"></span>
+
+                        <a class="btn btn-primary-outline prog"  data-nombre="Swift Ninja">Pedir programa y horarios</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>
